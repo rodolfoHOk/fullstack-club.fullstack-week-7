915e424b89714201054d9cac60d9e4e275c49af2
Rodolfo HiOk <rodolfo.okino@gmail.com>
2025-02-18 23:32:11 -0300

    chore: add eslint sort imports plugin and prettier tailwind plugin

--- donalds/src/app/layout.tsx
+++ donalds/src/app/layout.tsx
@@ -1,15 +1,16 @@
-import type { Metadata } from 'next';
-import { Poppins } from 'next/font/google';
-import './globals.css';
+import "./globals.css";
+
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
 
 const poppins = Poppins({
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  subsets: ['latin'],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: 'FSW Donalds',
-  description: 'Bora finalizar esse projeto lindo!',
+  title: "FSW Donalds",
+  description: "Bora finalizar esse projeto lindo!",
 };
 
 export default function RootLayout({
