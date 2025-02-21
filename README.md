e5292cd3c1ffe5e66f63e9e0529014027587f861
Rodolfo HiOk <rodolfo.okino@gmail.com>
2025-02-21 13:28:03 -0300

    feat: add cart item component

--- README.md
+++ README.md
@@ -19,3 +19,5 @@
 - [Figma Design](<https://www.figma.com/design/FIfajqda9slDT7JicbpzyE/FSW-Donald%E2%80%99s---Live-(Copy)?node-id=0-1&p=f&t=Dx2r0dHWASARiFup-0>)
 
 - [shadcn/ui](https://ui.shadcn.com/)
+
+- [Stripe Payment Gateway](https://stripe.com/br/payments)
--- donalds/src/app/[slug]/menu/components/cart-sheet.tsx
+++ donalds/src/app/[slug]/menu/components/cart-sheet.tsx
@@ -3,26 +3,25 @@ import { useContext } from 'react';
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from '@/components/ui/sheet';
 
 import { CartContext } from '../contexts/cart';
+import { CartItem } from './cart-item';
 
 export function CartSheet() {
   const { isOpen, toggleCart, products } = useContext(CartContext);
 
   return (
     <Sheet open={isOpen} onOpenChange={toggleCart}>
-      <SheetContent>
+      <SheetContent className="w-[80%]">
         <SheetHeader>
-          <SheetTitle>Sacola</SheetTitle>
-
-          <SheetDescription>
-            Seus produtos
-          </SheetDescription>
+          <SheetTitle className="text-left">Sacola</SheetTitle>
         </SheetHeader>
 
-        {products.map((product) => (
-          <h1 key={product.id}>
-            {product.name} - {product.quantity}
-          </h1>
-        ))}
+        <div className="flex h-full flex-col py-5">
+          <div className="flex-auto">
+            {products.map((product) => (
+              <CartItem key={product.id} product={product} />
+            ))}
+          </div>
+        </div>
       </SheetContent>
     </Sheet>
   );
